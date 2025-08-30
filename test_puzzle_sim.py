67182f8d2c4a1ad4df12b0a87d0089986891a74c
nihermann <hermann.nicolai@googlemail.com>
2025-08-30 13:42:05 +0200

    Added mem_save to tests

--- test_puzzle_sim.py
+++ test_puzzle_sim.py
@@ -8,21 +8,23 @@ from puzzle_sim import PuzzleSim, find_best_matching_piece
 device = 'gpu' if torch.cuda.is_available() else 'cpu'
 
 
-def test_same_input_yields_max_sim_in_puzzle_matching() -> None:
+@pytest.mark.parametrize("mem_save", [True, False])
+def test_same_input_yields_max_sim_in_puzzle_matching(mem_save: bool) -> None:
     priors = torch.rand(8, 3, 64, 64).to(device)
     test = priors[0]
 
-    sims = find_best_matching_piece(priors, test)
+    sims = find_best_matching_piece(priors, test, mem_save=mem_save)
 
     assert torch.allclose(sims, torch.ones_like(sims))
 
 
-def test_spatial_propagation_puzzle_matching() -> None:
+@pytest.mark.parametrize("mem_save", [True, False])
+def test_spatial_propagation_puzzle_matching(mem_save: bool) -> None:
     priors = torch.zeros((8, 3, 64, 64)).to(device)
     test = torch.ones_like(priors[0])
     priors[0,:,0,0] = 1  # set one pixel to white
 
-    sims = find_best_matching_piece(priors, test)
+    sims = find_best_matching_piece(priors, test, mem_save=mem_save)
 
     assert torch.allclose(sims, torch.ones_like(sims))
 
